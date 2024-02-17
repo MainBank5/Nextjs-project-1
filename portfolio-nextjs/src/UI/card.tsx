2ac53b25a6877ec0d2469676c3bc380fabd4ab2d
eliudkaruga <eliudkaruga97@gmail.com>
2024-02-17 17:13:25 +0300

    Readme update

--- portfolio-nextjs/src/UI/card.tsx
+++ portfolio-nextjs/src/UI/card.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 
-export default function SkillCard({ imageName, altText } : { imageName: string; altText?: string }) {
+export default function SkillCard({ imageName, altText } : { imageName: string; altText: string }) {
     return (
       <div className="flex justify-center items-center p-4 rounded-lg shadow-md">
         <Image src={imageName} alt={altText} width={80} height={80} />
